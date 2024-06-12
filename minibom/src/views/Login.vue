3fb3037b82c08cfcc7e3cfe36f582a131b749a6d
a011z <1874016044@qq.com>
2024-06-12 18:15:40 +0800

    提交

--- minibom/src/views/Login.vue
+++ minibom/src/views/Login.vue
@@ -30,41 +30,30 @@ const checkRePassword = (rule,value,callback)=>{
 const rules = ({
     username:[
         {required:true,message: '请输入用户名',trigger:'blur'},
-        {partern: /^[a-zA-Z0-9]{6,32}$/,
-            min: 6, max: 32, message: '长度为6~32位非空字符', trigger: 'blur'}
+        {partern: /^[a-zA-Z0-9]{6,32}$/,min: 6, max: 32, message: '长度为6~32位非空字符', trigger: 'blur'}
 
     ],
     telephone:[
         {required:true,message: '请输入电话号码',trigger:'blur'},
-        {partern:/^1[3-9]\\d{9}$/,
-            trigger:'blur'
-        }
+        {partern:/^1[3-9]\\d{9}$/,trigger:'blur'}
     ],
     email:[
         {required:true,message:"请输入邮箱"},
-        {partern:/^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/,
-            trigger:'blur'
-        }
-        {partern: /^[a-zA-Z0-9]{6,32}$/,
-            min: 6, max: 32, message: '长度为6~32位非空字符', trigger: 'blur'}
+        {partern:/^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/,trigger:'blur'},
+        {partern: /^[a-zA-Z0-9]{6,32}$/,min: 6, max: 32, message: '长度为6~32位非空字符', trigger: 'blur'}
 
     ],
     telephone:[
         {required:true,message: '请输入电话号码',trigger:'blur'},
-        {partern:/^1[3-9]\\d{9}$/,
-            trigger:'blur'
-        }
+        {partern:/^1[3-9]\\d{9}$/,trigger:'blur'}
     ],
     email:[
         {required:true,message:"请输入邮箱"},
-        {partern:/^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/,
-            trigger:'blur'
-        }
+        {partern:/^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/,trigger:'blur'}
     ],
     password:[
         {required:true,message: '请输入密码',trigger:'blur'},
-        {partern: /^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$/,
-        min: 8, max: 32, message: '长度为8~32位非空字符', trigger: 'blur'}
+        {partern: /^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$/,min: 8, max: 32, message: '长度为8~32位非空字符', trigger: 'blur'}
     ],
     rePassword:[
         {validator:checkRePassword,trigger: 'blur'}
