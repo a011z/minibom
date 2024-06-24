bcf5fce542835fb2cd06994a1013d4ea03c86b18
a011z <1874016044@qq.com>
2024-06-24 21:29:19 +0800

    bom清单完成

--- minibom/src/api/layout.js
+++ minibom/src/api/layout.js
@@ -25,7 +25,7 @@ export const subPartListService=(versionId)=>{
 export const AddSubpartService=(addSubPartModel)=>{    
     console.log(addSubPartModel);
 
-    return request.post('http://localhost:8080/BOMLink/create',addSubPartModel)
+    return request.post('/BOMLink/create',addSubPartModel)
 }
 
 
@@ -37,17 +37,25 @@ export const subPartUpdateService=(subPartModel)=>{//等待参数
         referenceDesignator: subPartModel.referenceDesignator
       };  
 
-    return request.put('http://localhost:8080/BOMLink/update',params)//等待路径
+    return request.put('/BOMLink/update',params)//等待路径
 }
 
 //删除子项
 export const subPartDeleteService=(BOMLinkId)=>{//等待参数id?,具体名字暂时未知
-    return request.delete('http://localhost:8080/BOMLink/delete/id='+BOMLinkId)//等待路径
+    return request.delete('/BOMLink/delete/id='+BOMLinkId)//等待路径
 }//还需要考虑
 
 
 //查询父项
 export const parentListService=(sourceName)=>{
 
-    return request.post('http://localhost:8080/BOMLink/queryParentItem',{sourceName})
+    return request.post('/BOMLink/queryParentItem',{sourceName})
 }
+
+//查看bom清单
+export const bomListService=(params)=>{
+
+    return request.get(`/BOMLink/listQuery/${params}`)
+
+}
+
